94a313647be1f3495808f77d10943b4f8a44ec87
Athina <athina.kantis@gmail.com>
2025-03-02 14:14:54 +0200

    add searched countries, filterCountriesBySearch function

--- frontend/src/store/slices/countriesSlice.ts
+++ frontend/src/store/slices/countriesSlice.ts
@@ -8,6 +8,8 @@ const initialState: CountryState = {
     loading: false,
     error: null,
     selectedCountry: null,
+    regionalCountries: [],
+    filteredCountries: []
 };
 
 export const fetchAllCountries = createAsyncThunk(
@@ -26,6 +28,14 @@ export const fetchOneCountry = createAsyncThunk(
     }
 );
 
+export const fetchRegionalCountries = createAsyncThunk(
+    'countries/fetchRegionalCountries',
+    async (region: string) => {
+        const response = await countriesApi.getRegionalCountries(region)
+        return response;
+    }
+)
+
 export const countriesSlice = createSlice({
     name: 'countries',
     initialState,
@@ -34,6 +44,12 @@ export const countriesSlice = createSlice({
             state.selectedCountry = null;
             state.error = null;
         },
+        filterCountriesBySearch: (state, action) => {
+            console.log(action.payload)
+            const searchterm = action.payload;
+            console.log(searchterm)
+            state.filteredCountries = (state.regionalCountries || state.countries).filter(country => country.name.common.toLowerCase().includes(searchterm))
+        }
     },
     extraReducers: (builder) => {
         builder.addCase(fetchAllCountries.pending, (state) => {
@@ -60,6 +76,18 @@ export const countriesSlice = createSlice({
             state.error =
                 (action.payload as string) || 'Failed to fetch country';
         });
+        builder.addCase(fetchRegionalCountries.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchRegionalCountries.fulfilled, (state, action) => {
+            state.loading = false;
+            state.regionalCountries = action.payload;
+        });
+        builder.addCase(fetchRegionalCountries.rejected, (state, action) => {
+            state.loading = false;
+            state.error =
+                (action.payload as string) || 'Failed to fetch countries';
+        });
     },
 });
 
@@ -73,5 +101,8 @@ export const selectCountriesError = (state: RootState) => state.countries.error;
 export const selectCountriesLoading = (state: RootState) =>
     state.countries.loading;
 
-export const { clearSelectedCountry } = countriesSlice.actions;
+export const selectRegionalCountries = (state: RootState) => state.countries.regionalCountries
+export const selectSearchedCountries = (state: RootState) => state.countries.filteredCountries
+
+export const { clearSelectedCountry, filterCountriesBySearch } = countriesSlice.actions;
 export default countriesSlice.reducer;
