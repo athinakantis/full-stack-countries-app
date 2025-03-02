f24632d55df04923299014d021a5c89336905a6d
Athina <athina.kantis@gmail.com>
2025-03-02 14:15:57 +0200

    update Login.tsx with custom styling

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -4,12 +4,9 @@ import { supabase } from "../../config/supabase";
 
 export const Login = () => {
   return (
-    <div className='login-container'
+    <div className='login-container flex justify-center'
     >
-      <div>
-        <h5>
-          Welcome
-        </h5>
+      <div className='mt-10 w-11/12 max-w-md'>
         <Auth
           supabaseClient={supabase}
           appearance={{
@@ -19,7 +16,22 @@ export const Login = () => {
                 colors: {
                   brand: "#1976d2",
                   brandAccent: "#1565c0",
+                  inputBackground: 'white',
+                  inputBorder: 'oklch(0.704 0.04 256.788)',
+                  inputBorderFocus: 'oklch(0.704 0.04 256.788)',
+                  anchorTextColor: 'oklch(0.446 0.043 257.281)',
+                  defaultButtonBackgroundHover: 'oklch(0.97 0.014 254.604)',
+                  defaultButtonBorder: 'oklch(0.704 0.04 256.788)',
+                  inputLabelText: 'oklch(0.554 0.046 257.417)',
                 },
+                fontSizes: {
+                  baseButtonSize: '1.1rem',
+                },
+                fonts: {
+                  buttonFontFamily: 'Lato',
+                  labelFontFamily: 'Lato',
+                  inputFontFamily: 'Lato'
+                }
               },
             },
           }}
