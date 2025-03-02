af6475cff9f776b881f94cdcc248902b3faafac8
Athina <athina.kantis@gmail.com>
2025-03-02 14:17:15 +0200

    add styling, loading and error

--- frontend/src/components/CountryDetail.tsx
+++ frontend/src/components/CountryDetail.tsx
@@ -31,17 +31,24 @@ export const SingleCountry = () => {
     }
 
     return (
-        <section id='single-country-container'>
+        <section id='single-country-container' className='flex gap-6 justify-center mx-auto pt-10 px-4 flex-wrap max-w-7xl'>
             {country && (
                 <>
-                    <div className='single-country'>
-                        <img src={country.flags.png} alt='' />
-                        <h1>{country.name.common}</h1>
-                        <p>{country.capital.join(', ')}</p>
-                        <p>{country.region}</p>
-                        <p>{country.subregion}</p>
+                    {/* <Link to='/countries' className='bg-blue-200 hover:bg-blue-300 h-fit px-5 py-2 radius-full transition-colors'>Back to countries</Link> */}
+                    <div className='single-country p-6  max-w-md shadow-sm rounded-sm bg-white dark:bg-slate-800 dark:text-slate-200 flex-grow'>
+                        <h1 className='text-3xl mb-4'>{country.name.common}</h1>
+
+                        <div className="text-md">
+                            <p className='mt-4'><span className='font-bold'>{country.capital.length > 1 ? 'Capitals: ' : 'Capital: '}</span>{country.capital.join(', ')}</p>
+                            <p><span className='font-bold'>Region:</span> {country.region}</p>
+                            <p><span className='font-bold'>Subregion:</span> {country.subregion}</p>
+                            <p><span className='font-bold'>Languages: </span>{country.languages ? Object.values(country.languages).join(', ') : 'N/A'}</p>
+                        </div>
                     </div>
-                    <WeatherInfo city={country.capital[0]} />
+                    <div className='flex gap-8 flex-col max-w-md'>
+                        <img src={country.flags.png} className='rounded-sm shadow-sm h-fit' alt='' />
+                        <WeatherInfo city={country.capital[0]} />
+                    </div>
                 </>
             )}
         </section>
