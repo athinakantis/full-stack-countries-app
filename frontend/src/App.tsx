945c5c7aa6f16c17a103193378ea7b6fabc27259
Athina <athina.kantis@gmail.com>
2025-03-02 14:12:59 +0200

    add initial home route + main styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,37 +8,42 @@ import { ProtectedTestData } from './components/Auth/ProtectedTestData';
 import { AuthRedirect } from './components/Auth/AuthRedirect';
 import { CountriesList } from './components/CountriesList';
 import { SingleCountry } from './components/CountryDetail';
+import { SearchProvider } from './context/SearchContext';
+import { Spinner } from './components/Spinner';
 
 function App() {
     return (
         <AuthProvider>
-            <BrowserRouter>
-                <Navigation />
-                <main>
-                    <Routes>
-                        <Route
-                            path='/login'
-                            element={
-                                <>
-                                    <AuthRedirect />
-                                    <Login />
-                                </>
-                            }
-                        />
-                        <Route path='/countries' element={<CountriesList />}/> 
-                        <Route path='/countries/:name' element={<SingleCountry />}/>,
-                        <Route path='/test' element={<TestData />} />
-                        <Route
-                            path='/protected'
-                            element={
-                                <ProtectedRoute>
-                                    <ProtectedTestData />
-                                </ProtectedRoute>
-                            }
-                        />
-                    </Routes>
-                </main>
-            </BrowserRouter>
+            <SearchProvider>
+                <BrowserRouter>
+                    <Navigation />
+                    <main className='bg-blue-100 dark:bg-slate-900 text-slate-800'>
+                        <Routes>
+                            <Route
+                                path='/login'
+                                element={
+                                    <>
+                                        <AuthRedirect />
+                                        <Login />
+                                    </>
+                                }
+                            />
+                            <Route path='/' element={<Spinner />} />
+                            <Route path='/countries' element={<CountriesList />} />
+                            <Route path='/countries/:name' element={<SingleCountry />} />,
+                            <Route path='/test' element={<TestData />} />
+                            <Route
+                                path='/protected'
+                                element={
+                                    <ProtectedRoute>
+                                        <ProtectedTestData />
+                                    </ProtectedRoute>
+                                }
+                            />
+                        </Routes>
+                    </main>
+                </BrowserRouter>
+            </SearchProvider>
         </AuthProvider>
     );
 }
