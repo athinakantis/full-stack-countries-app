3465db802ce60d1a97a1dbe7491bf8b410a4d37d
Athina <athina.kantis@gmail.com>
2025-03-03 15:02:32 +0200

    add Access-control-allow-origin header

--- frontend/src/api/services/countries.ts
+++ frontend/src/api/services/countries.ts
@@ -3,10 +3,14 @@ import { api } from '../axios';
 
 export const countriesApi = {
     getAllCountries: (): Promise<Country[]> =>
-        api.get('/all?fields=name,population,capital,region,flags'),
+        api.get('/all?fields=name,population,capital,region,flags,cca3',
+            {headers: {'Access-Control-Allow-Origin': '*'}}
+
+        ),
     getOneCountry: (name: string): Promise<Country[]> =>
         api.get(
-            `/name/${name}?fields=name,population,capital,region,tld,subregion,currencies,languages,borders,cca3,flags`
+            `/name/${name}?fields=name,population,capital,region,tld,subregion,currencies,languages,borders,cca3,flags,latlng`, 
+            {headers: {'Access-Control-Allow-Origin': '*'}}
         ),
     getRegionalCountries: (region: string): Promise<Country[]> => api.get(`/region/${region}`)
 };
