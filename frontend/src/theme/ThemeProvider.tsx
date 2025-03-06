198efa8fb1e44d9be13380e070f0652ef997cab5
Athina <athina.kantis@gmail.com>
2025-03-06 19:17:55 +0200

    update theme handling logic to check media preferences

--- frontend/src/theme/ThemeProvider.tsx
+++ frontend/src/theme/ThemeProvider.tsx
@@ -3,7 +3,7 @@ import { ThemeContext } from "./themeContext";
 import { Theme } from './themeContext';
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [currentTheme, setCurrentTheme] = useState<Theme>(localStorage.theme || window.matchMedia("(prefers-color-scheme: dark)"))
+  const [currentTheme, setCurrentTheme] = useState<Theme>(localStorage.theme || (window.matchMedia("(prefers-color-scheme: dark)") ? 'dark' : 'light'))
 
   const handleThemeSwitch = () => {
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
