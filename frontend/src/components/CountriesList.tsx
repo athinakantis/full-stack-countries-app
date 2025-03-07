2db51bc3a282803309ee42f7cf85424ff7d6107d
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-03-07 14:56:34 +0200

    extract card content into seperate component

--- frontend/src/components/CountriesList.tsx
+++ frontend/src/components/CountriesList.tsx
@@ -13,12 +13,11 @@ import {
     selectTotalPages,
 } from '../store/slices/countriesSlice';
 import { Country } from '../types/country';
-import { Link } from 'react-router-dom';
 import { useSearch } from '../context/SearchContext';
 import { Spinner } from './Spinner';
 import { Error } from './Error';
 import Pagination from '@mui/material/Pagination';
-import { formatPopulation } from '../utils/formatCountryData';
+import CountryCard from './CountryCard';
 
 export const CountriesList = () => {
     const { filter, search } = useSearch();
@@ -29,19 +28,23 @@ export const CountriesList = () => {
     const searchedCountries = useAppSelector(selectSearchedCountries);
     const loading = useAppSelector(selectCountriesLoading);
     const error = useAppSelector(selectCountriesError);
-    const [page, setPage] = useState(1)
-    const totalPages = useAppSelector(selectTotalPages)
+    const [page, setPage] = useState(1);
+    const totalPages = useAppSelector(selectTotalPages);
 
     useEffect(() => {
         if (allCountries.length < 1) dispatch(fetchAllCountries());
         async function updateDisplayedCountries() {
-            setPage(1)
+            setPage(1);
             if (search) {
-                if (regionalCountries.length < 1 || regionalCountries[0].region !== filter) await dispatch(fetchRegionalCountries(filter))
+                if (
+                    regionalCountries.length < 1 ||
+                    regionalCountries[0].region !== filter
+                )
+                    await dispatch(fetchRegionalCountries(filter));
                 dispatch(filterCountriesBySearch({ search, filter }));
             } else if (filter) {
                 await dispatch(fetchRegionalCountries(filter));
-            } else if (!filter && !search) dispatch(resetTotalPages())
+            } else if (!filter && !search) dispatch(resetTotalPages());
         }
         updateDisplayedCountries();
     }, [filter, search]);
@@ -52,15 +55,25 @@ export const CountriesList = () => {
         return array.slice(startIndex, endIndex);
     }
 
-    const handleChange = (_: React.ChangeEvent<unknown>, value: number) => {
+    const handlePaginate = (_: React.ChangeEvent<unknown>, value: number) => {
         setPage(value);
     };
 
     useEffect(() => {
-        if (search) setDisplayedCountries(pageinateCountries(searchedCountries));
-        if (filter && !search) setDisplayedCountries(pageinateCountries(regionalCountries));
-        if (!filter && !search) setDisplayedCountries(pageinateCountries(allCountries));
-    }, [allCountries, regionalCountries, searchedCountries, search, filter, page]);
+        if (search)
+            setDisplayedCountries(pageinateCountries(searchedCountries));
+        if (filter && !search)
+            setDisplayedCountries(pageinateCountries(regionalCountries));
+        if (!filter && !search)
+            setDisplayedCountries(pageinateCountries(allCountries));
+    }, [
+        allCountries,
+        regionalCountries,
+        searchedCountries,
+        search,
+        filter,
+        page,
+    ]);
 
     if (loading) return <Spinner />;
     if (error) return <Error error={error} />;
@@ -73,25 +86,7 @@ export const CountriesList = () => {
             >
                 {displayedCountries.length > 0 &&
                     displayedCountries.map((country: Country) => (
-                        <div
-                            key={country.cca3}
-                            className='country-card rounded-sm overflow-hidden transition-shadow hover:shadow-lg bg-white dark:text-slate-200 dark:bg-slate-800 h-fit transition-all'
-                        >
-                            <Link to={country.name.common}>
-                                <img
-                                    width={320}
-                                    src={country.flags.png}
-                                    alt={country.flags.alt}
-                                />
-                                <div className='p-3'>
-                                    <p className='text-lg font-semibold'>{country.name.common}</p>
-                                    <p>
-                                        <span className='font-medium'>Population: </span>
-                                        {formatPopulation(country.population)}
-                                    </p>
-                                </div>
-                            </Link>
-                        </div>
+                        <CountryCard key={country.cca3} country={country} />
                     ))}
                 {totalPages > 1 && (
                     <Pagination
@@ -99,7 +94,7 @@ export const CountriesList = () => {
                         count={totalPages}
                         color='primary'
                         page={page}
-                        onChange={handleChange}
+                        onChange={handlePaginate}
                     />
                 )}
             </div>
