785392d27be490f27ed07070c03d3bf725f5fdc4
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-03-07 14:56:44 +0200

    update Error

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 export const Error = ({ error }: { error: string }) => {
   const navigate = useNavigate()
   return (
-    <div>
+    <div className='dark:text-slate-200'>
       <h1>Uh-oh</h1>
       <p>It appears something went wrong</p>
       <Link to='/'>Home</Link>
