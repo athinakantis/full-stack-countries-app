d747d2d2fb29d7e55a942a6d7b97d5a0bf8c5d77
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-03-15 18:22:18 +0200

    add cypress config, update package json scripts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5180,
-  },
-})
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 5180,
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/setupTests.ts'],
+        css: false,
+    },
+});
