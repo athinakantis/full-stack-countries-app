969b660454ee164035643ce289ae017e4bb1e3bf
Athina <athina.kantis@gmail.com>
2025-03-17 09:55:39 +0200

    add formatCurrencies func

--- frontend/src/utils/formatCountryData.ts
+++ frontend/src/utils/formatCountryData.ts
@@ -1,3 +1,9 @@
+import { Currency } from '../types/country'
+
 export function formatPopulation(population: number): string {
   return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+export function formatCurrencies(currencies: Record<string, Currency>) {
+  return Object.values(currencies).map(currency => currency.name).join(', ')
 }
