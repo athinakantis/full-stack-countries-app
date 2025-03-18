9414b3ac2001b60341e2e5a4e880cdd5c2f5e8fa
Athina <athina.kantis@gmail.com>
2025-03-18 10:20:03 +0200

    update favorites test

--- frontend/cypress/e2e/favorites.cy.ts
+++ frontend/cypress/e2e/favorites.cy.ts
@@ -2,15 +2,16 @@ describe('Testing favorites', () => {
   beforeEach(() => cy.login().wait(1000))
 
   it('Can add countries as favorites', () => {
-    cy.visit('/countries').wait(4000)
-    cy.get('button[data-test-id="favorite-button"]').first().click().wait(2000)
+    cy.visit('/countries').wait(2000)
+    cy.get('button[data-test-id="favorite-button"]').first().click().wait(1000)
 
     cy.visit('/favorites').getCountryCards().should('have.length', 1)
   })
 
   it('Can remove from favorites', () => {
-    cy.visit('/favorites').wait(4000)
-    cy.get('button[data-test-id="favorite-button"]').first().click().wait(2000)
+    cy.visit('/favorites').wait(2000)
+    cy.get('button[data-test-id="favorite-button"]').first().click().wait(1000)
+    cy.reload()
     cy.getCountryCards().should('not.exist')
   })
 })
