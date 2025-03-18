f975a1e8997068dcfb2c973f7109c36545202354
Athina <athina.kantis@gmail.com>
2025-03-18 09:12:56 +0200

    add login command

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -26,30 +26,50 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
 declare global {
-    namespace Cypress {
-        interface Chainable {
-            login(email: string, password: string): Chainable<void>;
-            drag(
-                subject: string,
-                options?: Partial<TypeOptions>
-            ): Chainable<Element>;
-            dismiss(
-                subject: string,
-                options?: Partial<TypeOptions>
-            ): Chainable<Element>;
-            visit(
-                originalFn: CommandOriginalFn<any>,
-                url: string,
-                options: Partial<VisitOptions>
-            ): Chainable<Element>;
-            getCountryCards(index?: number): Chainable<JQuery<HTMLElement>>;
-        }
+  namespace Cypress {
+    interface Chainable {
+      login(): Chainable<void>;
+      drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>;
+      dismiss(
+        subject: string,
+        options?: Partial<TypeOptions>
+      ): Chainable<Element>;
+      visit(
+        originalFn: CommandOriginalFn<any>,
+        url: string,
+        options: Partial<VisitOptions>
+      ): Chainable<Element>;
+      getCountryCards(index?: number): Chainable<JQuery<HTMLElement>>;
     }
+  }
 }
 
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('getCountryCards', (index?: number) => {
-    const cards = cy.get('.country-card');
-    return typeof index === 'number' ? cards.eq(index) : cards;
+  const cards = cy.get('.country-card');
+  return typeof index === 'number' ? cards.eq(index) : cards;
+});
+
+Cypress.Commands.add('login', () => {
+//   cy.request({
+//     method: 'POST',
+//     url: `https://${Cypress.env(
+//       'SUPABASE_URL'
+//     )}/auth/v1/token?grant_type=password`,
+//     body: {
+//       email: Cypress.env('TEST_USER_EMAIL'),
+//       password: Cypress.env('TEST_USER_PW'),
+//     },
+//     headers: {
+//       apikey: Cypress.env('SUPABASE_ANON_KEY'),
+//     },
+//   }).then(({ body }) => {
+//     localStorage.setItem('supabase.auth.token', JSON.stringify(body));
+//   });
+
+cy.visit('/login')
+cy.get('#email').type(Cypress.env('TEST_USER_EMAIL'))
+cy.get('input[type="password"]').type(Cypress.env('TEST_USER_PW'))
+cy.get('button[type="submit"]').click()
 });
