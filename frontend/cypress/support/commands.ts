ae1956f116244a0cf5dc08ca5da06b422b3d0cf4
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-03-21 12:07:59 +0200

    format

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -26,55 +26,62 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
 declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(username: string, password: string): Chainable<void>;
-      drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>;
-      dismiss(
-        subject: string,
-        options?: Partial<TypeOptions>
-      ): Chainable<Element>;
-      visit(
-        originalFn: CommandOriginalFn<any>,
-        url: string,
-        options: Partial<VisitOptions>
-      ): Chainable<Element>;
-      getCountryCards(index?: number): Chainable<JQuery<HTMLElement>>;
+    namespace Cypress {
+        interface Chainable {
+            login(username: string, password: string): Chainable<void>;
+            drag(
+                subject: string,
+                options?: Partial<TypeOptions>
+            ): Chainable<Element>;
+            dismiss(
+                subject: string,
+                options?: Partial<TypeOptions>
+            ): Chainable<Element>;
+            visit(
+                originalFn: CommandOriginalFn<any>,
+                url: string,
+                options: Partial<VisitOptions>
+            ): Chainable<Element>;
+            getCountryCards(index?: number): Chainable<JQuery<HTMLElement>>;
+        }
     }
-  }
 }
 
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('getCountryCards', (index?: number) => {
-  const cards = cy.get('.country-card');
-  return typeof index === 'number' ? cards.eq(index) : cards;
+    const cards = cy.get('.country-card');
+    return typeof index === 'number' ? cards.eq(index) : cards;
 });
 
 Cypress.Commands.add('login', (username, password) => {
-  //   cy.request({
-  //     method: 'POST',
-  //     url: `https://${Cypress.env(
-  //       'SUPABASE_URL'
-  //     )}/auth/v1/token?grant_type=password`,
-  //     body: {
-  //       email: Cypress.env('TEST_USER_EMAIL'),
-  //       password: Cypress.env('TEST_USER_PW'),
-  //     },
-  //     headers: {
-  //       apikey: Cypress.env('SUPABASE_ANON_KEY'),
-  //     },
-  //   }).then(({ body }) => {
-  //     localStorage.setItem('supabase.auth.token', JSON.stringify(body));
-  //   });
+    //   cy.request({
+    //     method: 'POST',
+    //     url: `https://${Cypress.env(
+    //       'SUPABASE_URL'
+    //     )}/auth/*`,
+    //     body: {
+    //       email: Cypress.env('TEST_USER_EMAIL'),
+    //       password: Cypress.env('TEST_USER_PW'),
+    //     },
+    //     headers: {
+    //       apikey: Cypress.env('SUPABASE_ANON_KEY'),
+    //     },
+    //   }).then(({ body }) => {
+    //     localStorage.setItem('supabase.auth.token', JSON.stringify(body));
+    //   });
 
-  cy.session(`Session with ${username}`, () => {
-    cy.visit('/login');
-    cy.get('#email').type(username);
-    cy.get('input[type="password"]').type(password);
-    cy.get('button[type="submit"]').click()
-    cy.wait(1000)
-  }, {
-    cacheAcrossSpecs: true
-  })
+    cy.session(
+        `Session with ${username}`,
+        () => {
+            cy.visit('/login');
+            cy.get('#email').type(username);
+            cy.get('input[type="password"]').type(password);
+            cy.get('button[type="submit"]').click();
+            cy.wait(1000);
+        },
+        {
+            cacheAcrossSpecs: true,
+        }
+    );
 });
