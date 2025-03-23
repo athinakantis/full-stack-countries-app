74a822ffd83c9f326e066108d88680106b914f38
Athina <athina.kantis@gmail.com>
2025-03-23 13:17:14 +0200

    update home styling: ensure svgs do not shrink

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -8,8 +8,8 @@ export const Home = () => {
   return (
     <section
       id='home-container'
-      className='flex flex-wrap *:w-75 gap-6 justify-center pt-20 *:bg-blue-300 *:py-4 *:px-6 *:hover:cursor-pointer *:dark:bg-indigo-400 *:rounded-sm *:flex *:gap-7 *:items-center **:[span]:flex-grow
-      custom-hover-translate-y *:transition-all *:font-semibold'
+      className='flex flex-wrap *:w-78 gap-6 justify-center pt-20 *:bg-blue-300 *:py-4 *:px-6 *:hover:cursor-pointer *:dark:bg-indigo-400 *:rounded-sm *:flex *:gap-8 *:items-center
+      custom-hover-translate-y *:transition-all *:font-semibold **:[svg]:w-fit'
     >
       <Link to='/countries'>
         <Earth />
