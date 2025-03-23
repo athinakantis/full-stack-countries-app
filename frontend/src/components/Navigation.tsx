0163bd706b67c89f3095616dffc8861c84b069fa
Athina <athina.kantis@gmail.com>
2025-03-23 11:45:55 +0200

    update nav styling

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -11,7 +11,7 @@ export const Navigation = () => {
 
     return (
         <header className='w-full bg-blue-200 px-4 py-1 flex justify-center align-center dark:bg-slate-800'>
-            <div className="header-content max-w-screen-2xl w-11/12 flex justify-between">
+            <div className="header-content max-w-screen-2xl w-11/12 flex justify-between flex-wrap md:max-lg:text-sm">
                 <nav>
                     <ul className='*:px-3 [&>li:has(a.active)]:dark:bg-indigo-700 [&>li:has(a.active)]:bg-blue-300 *:py-1 flex p-2 gap-4 *:text-sm *:hover:bg-blue-300 **:hover:cursor-pointer  *:rounded-full *:transition-colors *:dark:bg-slate-800 *:py-1 *:dark:hover:bg-indigo-700 *:dark:text-slate-200'>
                         <li><NavLink to='/'>Home</NavLink></li>
