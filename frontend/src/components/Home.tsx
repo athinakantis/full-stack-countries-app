245a593e6f0b38b8ad45e03e493b9d7df1843301
Athina <athina.kantis@gmail.com>
2025-03-23 08:14:54 +0200

    add Home

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,31 @@
+import { Earth, Heart, ScanFace } from 'lucide-react';
+import { useAuth } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
+
+export const Home = () => {
+  const { user } = useAuth();
+
+  return (
+    <section
+      id='home-container'
+      className='flex flex-wrap *:w-70 gap-6 justify-center pt-20 *:bg-blue-300 *:py-4 *:px-6 *:hover:cursor-pointer *:dark:bg-indigo-500 *:rounded-sm *:flex *:gap-4 *:items-center **:[span]:w-11/12
+      custom-hover-translate-y *:transition-all *:text-center *:font-semibold'
+    >
+      <Link to='/countries'>
+        <Earth />
+        <span>Explore the countries of the world</span>
+      </Link>
+      {user ? (
+        <Link to='/favorites'>
+          <Heart />
+          <span>View favorites</span>
+        </Link>
+      ) : (
+        <Link to='/login'>
+          <ScanFace />
+          <span>Log in to add favorites</span>
+        </Link>
+      )}
+    </section>
+  );
+};
